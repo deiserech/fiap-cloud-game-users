edcca946f29f5c47cd1b693c7f0b44e958c3344c
deiserech <deiserech@outlook.com>
2026-01-02 14:50:18 -0300

    feat: add Category field to Games table and update appsettings

---
+++ src/FiapCloudGames.Infrastructure/Migrations/20260102174557_GameCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiapCloudGames.Users.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Games");
+        }
+    }
+}
